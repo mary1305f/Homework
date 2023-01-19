72384d4e9c622b1be6b8bcf0e58b06f2bef84ca5
mary1305f <mary1305del2003@gmail.com>
2023-01-19 18:40:02 -0500

    Projects

--- src/project/Project2.java
+++ src/project/Project2.java
@@ -5,10 +5,22 @@ import java.lang.String;
 
 public class Project2 {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         //Using Scanner create an array of countries.
         // When an array is created, retrieve all values from it and while retrieving
         // those values print capital for each country. (use 2 different loops).
+        String[]country=new String[3];
+        String[]capital=new String[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("country");
+            country[i]=scanner.next();
 
+            System.out.println("capital");
+            capital[i]=scanner.next();
+            System.out.print("the country "+country[i]+" is the capital of "+capital[i]+"\n ");
+
+        }
+        scanner.close();
 
     }
 
